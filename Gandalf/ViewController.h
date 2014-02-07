826aeaf09d63f0b1e2dfc384f022015f4c7a0a79
szimano <szimano@szimano.org>
2014-02-07 16:19:16 +0100

    use estimotes treat each estimote as a separate region to get per-beacon notifications show the beacon with highest rssi

--- Gandalf/ViewController.h
+++ Gandalf/ViewController.h
@@ -9,6 +9,10 @@
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
 
+static NSString *const ADAM_BEACON = @"2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6";
+static NSString *const TOMEK_BEACON = @"B0702880-A295-A8AB-F734-031A98A512DE";
+static NSString *const ESTIMOTE_BEACON = @"B9407F30-F5F8-466E-AFF9-25556B57FE6D";
+
 @interface ViewController : UIViewController<CLLocationManagerDelegate>
 
 @end
--- Gandalf/ViewController.m
+++ Gandalf/ViewController.m
@@ -9,10 +9,12 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property(weak, nonatomic) IBOutlet UIImageView *image;
 @property(weak, nonatomic) IBOutlet UILabel *label;
 @property(weak, nonatomic) IBOutlet UITextField *input;
 @property(strong, nonatomic) CLBeaconRegion *region;
 @property(strong, nonatomic) CLLocationManager *locationManager;
+@property(strong, nonatomic) NSNumber *currentMajor;
 @end
 
 @implementation ViewController
@@ -28,15 +30,32 @@
     self.locationManager = [CLLocationManager new];
     self.locationManager.delegate = self;
 
-    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:@"2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6"];
+    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:ESTIMOTE_BEACON];
 
-    self.region = [[CLBeaconRegion alloc] initWithProximityUUID:uuid identifier:@"com.softwaremill.gandalf"];
+    _region = [[CLBeaconRegion alloc] initWithProximityUUID:uuid identifier:@"com.softwaremill.gandalf"];
+    _region.notifyEntryStateOnDisplay = YES;
 
-    self.region.notifyEntryStateOnDisplay = YES;
+    CLBeaconRegion *region1 = [[CLBeaconRegion alloc] initWithProximityUUID:uuid major:2784 identifier:@"com.softwaremill.gandalf1"];
+    region1.notifyEntryStateOnDisplay = YES;
+
+    CLBeaconRegion *region2 = [[CLBeaconRegion alloc] initWithProximityUUID:uuid major:5728 identifier:@"com.softwaremill.gandalf2"];
+    region2.notifyEntryStateOnDisplay = YES;
+
+    CLBeaconRegion *region3 = [[CLBeaconRegion alloc] initWithProximityUUID:uuid major:17819 identifier:@"com.softwaremill.gandalf2"];
+    region3.notifyEntryStateOnDisplay = YES;
 
     [self.locationManager startMonitoringForRegion:_region];
-
     [self.locationManager requestStateForRegion:_region];
+
+    [self.locationManager startMonitoringForRegion:region1];
+    [self.locationManager requestStateForRegion:region1];
+
+    [self.locationManager startMonitoringForRegion:region2];
+    [self.locationManager requestStateForRegion:region2];
+
+    [self.locationManager startMonitoringForRegion:region3];
+    [self.locationManager requestStateForRegion:region3];
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -47,21 +66,89 @@
 - (void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region {
     [self.label setText:@"in region"];
     NSLog(@"in region");
+
+    [_label setText:@"in region"];
+
+    [_locationManager startRangingBeaconsInRegion:_region];
 }
 
 - (void)locationManager:(CLLocationManager *)manager didExitRegion:(CLRegion *)region {
-    [self.label setText:@"out region"];
+    [self.label setText:@"left region"];
     NSLog(@"out region");
+
+    [self clearImage];
+
+    [_locationManager stopRangingBeaconsInRegion:_region];
 }
+
+- (void)locationManager:(CLLocationManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(CLBeaconRegion *)region {
+    NSLog(@"Ranged beacons: %@", beacons);
+
+    CLBeacon *closest = nil;
+    int maxRssi = -1000;
+
+    for (CLBeacon *beacon in beacons) {
+        if (beacon.proximity == CLProximityImmediate || beacon.proximity == CLProximityNear) {
+            if (beacon.rssi > maxRssi) {
+                closest = beacon;
+            }
+        }
+    }
+
+    if (closest != nil) {
+        NSLog(@"Closest: %@", closest);
+        [self loadImage:closest.major];
+    } else {
+        NSLog(@"Clear");
+        [self clearImage];
+    }
+}
+
+- (void)loadImage:(NSNumber *)major {
+    if (_currentMajor.intValue != major.intValue) {
+        NSLog(@"change %@ %@", _currentMajor, major);
+
+        _currentMajor = major;
+        NSMutableURLRequest *request = [NSMutableURLRequest
+                requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://warski.org/gandalf/%@.json", major]]];
+
+        [request setHTTPMethod:@"GET"];
+
+        NSError *error = nil;
+        NSURLResponse *urlResponse = nil;
+
+        NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&urlResponse error:&error];
+
+        NSLog(@"Got response %@", urlResponse);
+        NSLog(@"Got error %@", error);
+        NSLog(@"Got data %@", [NSString stringWithUTF8String:data.bytes]);
+
+        NSArray *json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+
+        NSLog(@"Got json: %@", json);
+        NSLog(@"Got name: %@", [json valueForKey:@"name"]);
+
+        UILocalNotification *notification = [[UILocalNotification alloc] init];
+        notification.alertBody = [NSString stringWithFormat:@"Found %@ near by! Watch out.", [json valueForKey:@"name"]];
+        notification.soundName = UILocalNotificationDefaultSoundName;
+
+        [[UIApplication sharedApplication] presentLocalNotificationNow:notification];
+
+        NSData *imageData = [[NSData alloc] initWithContentsOfURL:[NSURL URLWithString:[json valueForKey:@"pic"]]];
+        UIImage *image = [UIImage imageWithData:imageData];
+
+        [_image setImage:image];
+    }
+}
+
+- (void)clearImage {
+    [_image setImage: nil];
+    [_label setText: @""];
+}
+
 
 - (void)locationManager:(CLLocationManager *)manager didDetermineState:(CLRegionState)state forRegion:(CLRegion *)region {
     NSLog(@"Determined state %d for region %@", state, region);
-
-    UILocalNotification *notification = [[UILocalNotification alloc] init];
-    notification.alertBody = [NSString stringWithFormat:@"Region change %d", state];
-    notification.soundName = UILocalNotificationDefaultSoundName;
-
-    [[UIApplication sharedApplication] presentLocalNotificationNow:notification];
 }
 
 
